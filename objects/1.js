2556db9fe01d9f2d3267d0d1020aa6d914c7c3f9
mohdafnaan <mohdafnaan833@gmail.com>
2025-11-05 21:22:27 +0000

    objects file updated

---
+++ objects/1.js
@@ -0,0 +1,19 @@
+let details = {
+    fname : "afnaaan",
+    age : 20 ,
+    phnNo : 9573503729
+}
+
+console.log(details.fname);
+
+console.log(details.city = "hyderabad");
+
+console.log(details.phnNo = 9542053623);
+
+console.log(delete details.fname);
+console.log(details);
+
+
+
+
+
---
+++ objects/2.js
@@ -0,0 +1,26 @@
+let school = {
+    sname : "aimen",
+    rollno : 21,
+    isAlive : false
+}
+
+
+// view
+school.sname;
+console.log(school);
+
+// add
+
+school.sec = "c";
+
+console.log(school);
+
+// update
+
+ school.sec = "d"
+console.log(school);
+
+//delete 
+
+delete school.sec;
+console.log(school);
